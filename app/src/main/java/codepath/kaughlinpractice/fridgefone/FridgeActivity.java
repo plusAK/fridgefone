f18467c0eed3e15a66c7524cd6c164b253fd5e80
Avni Prasad <avniprasad@fb.com>
2018-07-16 14:51:31 -0700

    testing

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/FridgeActivity.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/FridgeActivity.java
@@ -9,5 +9,8 @@ public class FridgeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge);
+
+        // TESTING
+        
     }
 }
