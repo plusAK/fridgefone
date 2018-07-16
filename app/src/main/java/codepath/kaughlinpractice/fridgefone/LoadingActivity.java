f25eccba6a7afb030d998e893e44362b616f755e
Kaughlin Caver <kaughlin@fb.com>
2018-07-16 16:04:59 -0700

    deleted test

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/LoadingActivity.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/LoadingActivity.java
@@ -1,5 +1,6 @@
 package codepath.kaughlinpractice.fridgefone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        Thread mythread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent i = new Intent(getApplicationContext(), FridgeActivity.class);
+                    startActivity(i);
+                    finish();// destroys activity after it is used
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mythread.start();
     }
 }
