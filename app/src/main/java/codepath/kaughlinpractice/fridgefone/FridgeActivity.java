a8c8d9ca34004421ee59638f0874151bdb937292
Avni Prasad <avniprasad@fb.com>
2018-07-16 15:00:19 -0700

    update gitignore

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/FridgeActivity.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/FridgeActivity.java
@@ -10,7 +10,6 @@ public class FridgeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge);
 
-        // TESTING
-        
+
     }
 }
