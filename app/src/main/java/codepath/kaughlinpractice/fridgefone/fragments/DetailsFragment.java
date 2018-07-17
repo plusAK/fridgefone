de33a75634aec109db06fa688ae211d2911904e6
Katherine Arackaparambil <katherineara@fb.com>
2018-07-17 15:53:34 -0700

    Added fragments and deleted extra code

---
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/DetailsFragment.java
@@ -0,0 +1,20 @@
+package codepath.kaughlinpractice.fridgefone.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import codepath.kaughlinpractice.fridgefone.R;
+
+
+public class DetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+}
---
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/ListFragment.java
@@ -0,0 +1,19 @@
+package codepath.kaughlinpractice.fridgefone.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import codepath.kaughlinpractice.fridgefone.R;
+
+public class ListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
