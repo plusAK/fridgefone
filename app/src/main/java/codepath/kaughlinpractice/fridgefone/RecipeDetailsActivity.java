a18a71d75738e090d96f0b77bd025b6204dca590
Katherine Arackaparambil <katherineara@fb.com>
2018-07-16 17:31:34 -0700

    Updated XML for DetailsActivity and binded views to Java using ButterKnife

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/RecipeDetailsActivity.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/RecipeDetailsActivity.java
@@ -1,13 +1,26 @@
 package codepath.kaughlinpractice.fridgefone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivRecipeImage) public ImageView ivRecipeImage;
+    @BindView(R.id.tvDishTitle) public TextView tvDishTitle;
+    @BindView(R.id.tvIngredients) public TextView tvIngredients;
+    @BindView(R.id.tvInstructions) public TextView tvInstructions;
+    @BindView(R.id.buttonBack) public Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+        ButterKnife.bind(this);
     }
 }
