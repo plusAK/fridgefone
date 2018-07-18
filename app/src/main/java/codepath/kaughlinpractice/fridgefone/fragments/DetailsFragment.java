3b39760a36fd90cda2e1a0dc5902e1a9ee4163d2
Katherine Arackaparambil <katherineara@fb.com>
2018-07-18 10:30:41 -0700

    Fixed ButterKnife for Fragments

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/DetailsFragment.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/DetailsFragment.java
@@ -8,8 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import codepath.kaughlinpractice.fridgefone.MainActivity;
 import codepath.kaughlinpractice.fridgefone.R;
 import codepath.kaughlinpractice.fridgefone.model.Recipe;
@@ -18,40 +21,32 @@ import codepath.kaughlinpractice.fridgefone.model.Recipe;
 public class DetailsFragment extends Fragment {
 
     public Recipe recipe;
-    /*
     @BindView(R.id.ivRecipeImage) public ImageView ivRecipeImage;
     @BindView(R.id.tvDishTitle) public TextView tvDishTitle;
     @BindView(R.id.tvIngredients) public TextView tvIngredients;
     @BindView(R.id.tvInstructions) public TextView tvInstructions;
     @BindView(R.id.buttonBack) public Button buttonBack;
-    */
-    public TextView tvDishTitle;
-    public Button buttonBack;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // ButterKnife.bind(getActivity());
-        // TODO -- fix ButterKnife to work here
-        tvDishTitle = view.findViewById(R.id.tvDishTitle);
-        buttonBack = view.findViewById(R.id.buttonBack);
-
-
         //recipe = (Recipe) Parcels.unwrap(getIntent().getParcelableExtra(Recipe.class.getSimpleName()));
         Bundle args = getArguments();
         String name = args.getString("name");
         tvDishTitle.setText(name);
-        //TODO change when bundle recieves object id
+//      TODO change when bundle receives object id
 
-        //  TODO: Add the image url into load() once we get it from API
+//        TODO: Add the image url into load() once we get it from API
 //        GlideApp.with(this)
 //                .load()
 //                .into(ivRecipeImage);
