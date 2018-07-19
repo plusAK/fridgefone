db101de42ae78a4d9b765a69d6bd2df7f8c4a929
Avni Prasad <avniprasad@fb.com>
2018-07-18 17:23:47 -0700

    Shaping recipes

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/model/Recipe.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/model/Recipe.java
@@ -1,5 +1,7 @@
 package codepath.kaughlinpractice.fridgefone.model;
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
@@ -24,32 +26,33 @@ public class Recipe {
 
     public static Recipe fromJSON(JSONObject jsonObject) throws JSONException {
         Recipe recipe = new Recipe();
-
         recipe.name = jsonObject.getString("title");
-        // TODO -- figure out how to deal with some objects not having these attributes
-        // recipe.name = "FOOD NAME";
-//        recipe.id = jsonObject.getInt("id");
-//        recipe.image = jsonObject.getString("image");
-//        recipe.readyInMinutes = jsonObject.getInt("readyInMinutes");
-//        recipe.vegetarian = jsonObject.getBoolean("vegetarian");
-//        recipe.vegan = jsonObject.getBoolean("vegan");
-//        recipe.glutenFree = jsonObject.getBoolean("glutenFree");
-//        recipe.dairyFree = jsonObject.getBoolean("dairyFree");
-//        recipe.veryHealthy = jsonObject.getBoolean("veryHealthy");
-//        recipe.cheap = jsonObject.getBoolean("cheap");
-//        recipe.veryPopular = jsonObject.getBoolean("veryPopular");
-//        recipe.servings = jsonObject.getInt("servings");
-        // recipe.ingredients = jsonObject.getJSONObject("extendedIngredients");
+        recipe.id = jsonObject.getInt("id");
+        recipe.image = jsonObject.getString("image");
+        Log.d("Recipe", "Have access to basic Recipe Info");
 
+        // GET Recipe from Ingredients API only gives name, id, image
+        if (jsonObject.has("readyInMinutes")) {
+            Log.d("Recipe", "Have access to additional Recipe Info");
+            // GET Recipe Information gives additional information about recipe
+            recipe.readyInMinutes = jsonObject.getInt("readyInMinutes");
+            recipe.vegetarian = jsonObject.getBoolean("vegetarian");
+            recipe.vegan = jsonObject.getBoolean("vegan");
+            recipe.glutenFree = jsonObject.getBoolean("glutenFree");
+            recipe.dairyFree = jsonObject.getBoolean("dairyFree");
+            recipe.veryHealthy = jsonObject.getBoolean("veryHealthy");
+            recipe.cheap = jsonObject.getBoolean("cheap");
+            recipe.veryPopular = jsonObject.getBoolean("veryPopular");
+            recipe.servings = jsonObject.getInt("servings");
+            // recipe.ingredients = jsonObject.getJSONObject("extendedIngredients");
+        }
         return recipe;
     }
 
     // this is for testing purposes
     public static Recipe fromString(String name) {
         Recipe recipe = new Recipe();
-
         recipe.name = name;
-
         return recipe;
     }
 
