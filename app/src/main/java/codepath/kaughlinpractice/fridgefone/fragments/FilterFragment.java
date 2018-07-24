da407f3210851520e7ebb1cc8ea6f06b511d8481
Katherine Arackaparambil <katherineara@fb.com>
2018-07-24 10:10:26 -0700

    Dismiss dialog when hit save

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/FilterFragment.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/FilterFragment.java
@@ -1,18 +1,32 @@
 package codepath.kaughlinpractice.fridgefone.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import codepath.kaughlinpractice.fridgefone.R;
 
 public class FilterFragment extends DialogFragment {
 
+    private Button mSaveButton;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mSaveButton = (Button) view.findViewById(R.id.saveButton);
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
     }
 
     @Override
@@ -20,5 +34,10 @@ public class FilterFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_filter, container, false);
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
     }
 }
