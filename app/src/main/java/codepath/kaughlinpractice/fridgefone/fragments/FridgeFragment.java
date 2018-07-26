cccc92878e5ca25040535effad30849c5588b67e
Katherine Arackaparambil <katherineara@fb.com>
2018-07-26 15:16:13 -0700

    No repeats in list of fridge items sent to the API

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/FridgeFragment.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/fragments/FridgeFragment.java
@@ -192,6 +192,8 @@ public class FridgeFragment extends Fragment{
 
     public void loadItems() {
 
+        fridge_items = "";
+
         final Item.Query itemsQuery = new Item.Query();
 
         itemsQuery.findInBackground(new FindCallback<Item>()
