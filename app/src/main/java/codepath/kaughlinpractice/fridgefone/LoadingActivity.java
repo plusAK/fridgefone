46ab259fd5d73cbf515289eff09d7e9ed73c1018
Avni Prasad <avniprasad@fb.com>
2018-08-09 21:45:20 -0700

    changed the loading screen ui

--- app/src/main/java/codepath/kaughlinpractice/fridgefone/LoadingActivity.java
+++ app/src/main/java/codepath/kaughlinpractice/fridgefone/LoadingActivity.java
@@ -21,15 +21,15 @@ public class LoadingActivity extends AwesomeSplash {
         /* you don't have to override every property */
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.loading_page_color); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setBackgroundColor(R.color.theme_peach); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1700); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
         //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
 
         //Customize Logo
-        configSplash.setLogoSplash(R.drawable.salt_bae); //or any other drawable
+        configSplash.setLogoSplash(R.drawable.peach_salt_bae); //or any other drawable
         configSplash.setAnimLogoSplashDuration(2000); //int ms
         configSplash.setAnimLogoSplashTechnique(Techniques.Shake); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
